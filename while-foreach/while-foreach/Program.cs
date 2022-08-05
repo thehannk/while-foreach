f90a1c7930fbb298b4ac82b44ae2bf0d03993a7b
thehannk <108367955+thehannk@users.noreply.github.com>
2022-08-05 14:14:15 +0300

    A while loop statement in C# repeatedly executes a statement as long as a given condition is true.A foreach loop in C# is used to loop over elements in an enumerable object.

---
+++ while-foreach/while-foreach/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            //birden başlayıp consoledan girilen sayıya kadar (sayı dahil) ortalama hesaplayıp consolea yazdırılması
+
+            Console.WriteLine("Lütfen sayı giriniz :");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a dan z e kadar tüm harfleri consolea yazdır.
+            char character = 'a';
+            while (character < z)
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            //foreach 
+
+            string[] arabalar = { "Ford", "Renault", "Maserati" };
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
